e035949c2cb02a16ba8056c643251712dcb2da09
Tobias Barth <4nduril@gmail.com>
2017-03-24 12:16:02 +0100

    Setup code and tests

---
+++ src/anonymizeIp.js
@@ -0,0 +1,16 @@
+const ip = require('ip');
+
+const anonymizeIp = remoteAddress => {
+  if (ip.isV4Format(remoteAddress)) {
+    const parts = remoteAddress.split('.');
+    return `${parts[0]}.${parts[1]}.0.0`;
+  }
+  if (ip.isV6Format(remoteAddress)) {
+    return (/::/.test(remoteAddress))
+      ? remoteAddress.replace(/(::).*/, '$1')
+      : remoteAddress.replace(/([a-f0-9]{1,4}:?){4}$/, ':');
+  }
+  return '::';
+};
+
+module.exports = anonymizeIp;
