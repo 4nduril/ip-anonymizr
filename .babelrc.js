968ad425bb736f0f76563dae884e34bff28af275
Tobias Barth <4nduril@gmail.com>
2018-10-23 10:04:56 +0200

    chore(Babel 7): Upgrade Babel to version 7^

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,12 @@
-const { BABEL_ENV, NODE_ENV } = process.env;
-
-const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'cjs';
-const test = BABEL_ENV === 'test' || NODE_ENV === 'test';
-
 module.exports = {
 	presets: [
-		[ 'env', { modules: false } ],
+		[ '@babel/preset-env', { modules: false } ],
 	],
-	plugins: [
-		(cjs || test) && 'transform-es2015-modules-commonjs',
-	].filter(Boolean),
+	env: {
+		test: {
+			presets: [
+				[ '@babel/preset-env' ],
+			],
+		}
+	}
 };
